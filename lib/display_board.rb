50cc94199eaaaa8695744c87ee1f83a9dc64d4ab
Greg Benn <gbenn@powerhrg.com>
2018-04-25 19:33:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,35 +7,4 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
 end
-# 
-# board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-# display_board(board)
-
-#board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
-#display_board(board)
-
-#board = [" O ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-#display_board(board)
-
-#board = [" O ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
-#display_board(board)
-
-#board = [" X ", " X ", " X ", "   ", "   ", "   ", "   ", "   ", "   "]
-#display_board(board)
-
-#board = ["   ", "   ", "   ", "   ", "   ", "   ", " O ", " O ", " O "]
-#display_board(board)
-
-#board = [" X ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", " X "]
-#display_board(board)
-
-#board = ["   ", "   ", " O ", "   ", " O ", "   ", " O ", "   ", "   "]
-#display_board(board)
-
-#board = [" X ", " X ", " X ", " X ", " O ", " O ", " X ", " O ", " O "]
-#display_board(board)
-
-#board = [" X ", " O ", " X ", " O ", " X ", " X ", " O ", " X ", " O "]
-#display_board(board)
